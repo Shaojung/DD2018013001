34bcb9f0ab4479dfcea92ffcc0d2ae2baecca99a
Teacher <shaojung@gmail.com>
2018-01-30 14:24:06 +0800

    Location

--- app/src/main/java/tw/com/pcschool/dd2018013001/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/dd2018013001/MainActivity.java
@@ -2,6 +2,8 @@ package tw.com.pcschool.dd2018013001;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -11,6 +13,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+
+import java.io.IOException;
+import java.util.List;
 
 import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
@@ -73,33 +78,46 @@ public class MainActivity extends AppCompatActivity {
         }
         lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new MyListener());
     }
-}
-class MyListener implements LocationListener {
+    class MyListener implements LocationListener {
 
-    @Override
-    public void onLocationChanged(Location location) {
-        Log.d("LOC", "Change!! " + location.getLatitude() + "," + location.getLongitude());
-        Location loc101 = new Location("LOC");
-        loc101.setLatitude(25.0336);
-        loc101.setLongitude(121.5646);
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.d("LOC", "Change!! " + location.getLatitude() + "," + location.getLongitude());
+            Location loc101 = new Location("LOC");
+            loc101.setLatitude(25.0336);
+            loc101.setLongitude(121.5646);
 
-        float dist = location.distanceTo(loc101);
-        Log.d("LOC", "Dist:" + dist);
+            float dist = location.distanceTo(loc101);
+            Log.d("LOC", "Dist:" + dist);
 
+            Geocoder geocoder = new Geocoder(MainActivity.this);
+            try {
+                List<Address> mylist = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                Address addr = mylist.get(0);
+                Log.d("LOC", addr.getAddressLine(0));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
     }
 
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-
-    }
 }
